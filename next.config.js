fad68ee3b705c5f80a7055a11ea79fe573e4d387
georgevela-papi <georgevela6@gmail.com>
2026-02-02 05:02:42 -0500

    Add files via upload
    fix pass: root config files

--- next.config.js
+++ next.config.js
@@ -5,10 +5,9 @@ const nextConfig = {
       {
         protocol: 'https',
         hostname: 'interstatetire.online',
-        pathname: '/**',
       },
     ],
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
